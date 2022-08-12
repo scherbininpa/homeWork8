e10ec1fc523183f4289bbeded3e8d2c384c0d16f
pa.shcherbinin <shcherbininpa@gmail.com>
2022-08-12 21:35:30 +0500

    add param msg in function Print. create region Task1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using homeWork8;
-
-Console.WriteLine("Hello, World!");
+#region Задание 1. Работа с листом.
+Console.WriteLine("Задание 1. Работа с листом.");
 Task1 task1 = new Task1(100);
-task1.Print();
+task1.Print("Начальный набор данных: ");
+task1.RemoveElementBetween(25, 50);
+task1.Print($"Набор элементов после удаления чисел между 25 и 50: ");
+Console.ReadLine();
+#endregion
--- task1.cs
+++ task1.cs
@@ -8,23 +8,40 @@ namespace homeWork8
 {
     struct Task1
     {
-        public List<int> list = new List<int>();
+        private List<int> list = new List<int>();
         public Task1(int count)
         {
             Random random = new Random();
-            for (int i = 1; i < count; i++)
+            for (int i = 1; i < count; i++) list.Add(random.Next(100));   
+        }
+        /// <summary>
+        /// Будут удалены элементы между минимадбным и максимальным значением
+        /// </summary>
+        /// <param name="minValue">Минимальное значение</param>
+        /// <param name="maxValue">Максимальное значение</param>
+        public void RemoveElementBetween(int minValue, int maxValue)
+        {
+            for (int i = 0; i < list.Count; i++)
             {
-                list.Add(random.Next(100));
+                if ( list[i]>minValue && list[i] < maxValue)
+                { 
+                    list.RemoveAt(i);
+                    i--;
+                }
             }
         }
-
-        public void Print()
+        /// <summary>
+        /// Вывод данных в консоль
+        /// </summary>
+        /// <param name="msg">Сообщение перед выводом данных</param>
+        public void Print(string msg)
         {
-            Console.WriteLine(list.Count);
+            Console.WriteLine($"{msg}");
             for (int i = 0; i < list.Count; i++)
             {
                 Console.Write((i == list.Count-1) ? $"{list[i]}" : $"{list[i]},");
             }
+            Console.WriteLine(Environment.NewLine);
         }
     }
 }
