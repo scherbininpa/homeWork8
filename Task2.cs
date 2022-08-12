85af278f36331d6baae2c0c502be887bcb42acba
pa.shcherbinin <shcherbininpa@gmail.com>
2022-08-12 21:52:52 +0500

    create struct Task2 and add function FillDictionary

---
+++ Task2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork8
+{
+    struct Task2
+    {
+        private Dictionary<long,string> dictionary = new Dictionary<long,string>();
+        public Task2()
+        { 
+        
+        }
+        public void FillDictionary()
+        {
+            bool endData = true;
+            while (endData)
+            {
+                Console.WriteLine("Введите номер телефона:");
+                long telepfone = long.Parse( Console.ReadLine());
+                if (telepfone == 0)
+                {
+                    endData = false;
+                }else
+                {
+                    Console.WriteLine("Введите ФИО:");
+                    string name = Console.ReadLine();
+                    dictionary.Add(telepfone, name);
+                }
+            }
+        }
+    }
+}
